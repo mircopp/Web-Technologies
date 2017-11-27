5977b9e23163e699ad5ac01939aefab81660b977
Mirco Pyrtek <mirco.pyrtek@dfki.de>
2017-11-27 16:28:13 +0100

    changed audience

--- Session 3 Web based authorization with OAuth 2.0/exercise/app.js
+++ Session 3 Web based authorization with OAuth 2.0/exercise/app.js
@@ -8,7 +8,7 @@ window.addEventListener('load', function() {
     domain: AUTH0_DOMAIN,
     clientID: AUTH0_CLIENT_ID,
     redirectUri: AUTH0_CALLBACK_URL,
-    audience: 'https://' + AUTH0_DOMAIN + '/userinfo',
+    audience: 'http://localhost:3000',
     responseType: 'token id_token',
     scope: 'openid email profile',
     leeway: 60
